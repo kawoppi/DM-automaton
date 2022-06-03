f6fda6b9e695c46c6d0ff70c199bbd00db3d6e12
Kawoppi <nwalsmits@outlook.com>
2022-06-03 16:48:40 +0200

    Added base automaton and transition

---
+++ DM-automaton/Automaton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM_automaton
+{
+	public class Automaton<TState, TSymbol>
+		where TState : IComparable<TState>
+	{
+		private ISet<Transition<TState, TSymbol>> transitions;
+		private SortedSet<TState> states;
+		private SortedSet<TState> startStates;
+		private SortedSet<TState> finalStates;
+		private SortedSet<TSymbol> symbols;
+
+		public Automaton(SortedSet<TSymbol> symbols)
+		{
+			this.transitions = new SortedSet<Transition<TState, TSymbol>>();
+			this.states = new SortedSet<TState>();
+			this.startStates = new SortedSet<TState>();
+			this.finalStates = new SortedSet<TState>();
+			this.symbols = symbols;
+		}
+	}
+}
---
+++ DM-automaton/Transition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM_automaton
+{
+	public class Transition<TState, TSymbol> : IComparable<Transition<TState, TSymbol>>
+		where TState : IComparable<TState>
+	{
+		private TState fromState;
+		private TSymbol symbol;
+		private TState toState;
+
+		public Transition(TState from, TSymbol symbol, TState to)
+		{
+			this.fromState = from;
+			this.symbol = symbol;
+			this.toState = to;
+		}
+
+		public int CompareTo(Transition<TState, TSymbol>? other)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
