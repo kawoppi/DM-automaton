9ea4367e17a7d86c77020edd6df37477ddaa18ad
Kawoppi <nwalsmits@outlook.com>
2022-06-03 15:52:48 +0200

    added TODOs

--- DM-automaton/Program.cs
+++ DM-automaton/Program.cs
@@ -10,3 +10,18 @@ namespace Automaton
 		}
 	}
 }
+
+/* TODO:
+ * design alphabet structure
+ * port automaton code
+ * convert NDFA to DFA automatically
+ * (create automaton unit tests)
+ * 
+ * finish grammar definition
+ * read DM file line by line
+ * 
+ * create DM definition datastructures
+ * have automaton create DM definions
+ * organize a collection of DM definitions into a tree structure
+ * display this tree structure
+ */
